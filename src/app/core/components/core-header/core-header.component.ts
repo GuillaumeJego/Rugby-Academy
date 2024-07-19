3026197732ae921321fa866b4fd612d733b23f5b
Diego <jego.guillaume22@gmail.com>
2024-07-19 22:20:27 +0200

    Add core & assets figma

---
+++ src/app/core/components/core-header/core-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './core-header.component.html',
+  styleUrl: './core-header.component.scss'
+})
+export class CoreHeaderComponent {
+
+}
---
+++ src/app/core/components/core-main/core-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './core-main.component.html',
+  styleUrl: './core-main.component.scss'
+})
+export class CoreMainComponent {
+
+}
