81defaa8c2783bb30a33da868961e91398f843c8
Diego <jego.guillaume22@gmail.com>
2025-04-09 18:41:59 +0200

    CRUD ok

--- src/app/core/services/test.service.ts
+++ src/app/core/services/test.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // Importer HttpHeaders
 import { Observable } from 'rxjs';
 import { TestModel } from '../models/test.model';
 
@@ -7,23 +7,27 @@ import { TestModel } from '../models/test.model';
   providedIn: 'root'
 })
 export class TestService {
-    private apiUrl = 'https://www.metana.fr/api/tests.php'; // Une seule URL pour toutes les actions
-  
-    constructor(private http: HttpClient) {}
-  
-    getTests(): Observable<TestModel[]> {
-      return this.http.get<TestModel[]>(this.apiUrl);
-    }
-  
-    addTest(test: Omit<TestModel, 'id'>): Observable<any> {
-      return this.http.post(this.apiUrl, test);
-    }
-  
-    updateTest(test: TestModel): Observable<any> {
-      return this.http.put(this.apiUrl, test);
-    }
-  
-    deleteTest(id: number): Observable<any> {
-      return this.http.delete(this.apiUrl, { body: { id } });
-    }
+  private apiUrl = '/api/tests.php'; // Une seule URL pour toutes les actions
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' }); // Définir le header
+
+  constructor(private http: HttpClient) {}
+
+  getTests(): Observable<TestModel[]> {
+    return this.http.get<TestModel[]>(this.apiUrl);
   }
+
+  addTest(test: Omit<TestModel, 'id'>): Observable<any> {
+    console.log('Envoi POST avec :', test); // Ajout de log pour déboguer
+    return this.http.post(this.apiUrl, test, { headers: this.headers });
+  }
+
+  updateTest(test: TestModel): Observable<any> {
+    console.log('Envoi PUT avec :', test); // Ajout de log
+    return this.http.put(this.apiUrl, test, { headers: this.headers });
+  }
+
+  deleteTest(id: number): Observable<any> {
+    console.log('Envoi DELETE avec ID :', id); // Ajout de log
+    return this.http.delete(this.apiUrl, { headers: this.headers, body: { id } });
+  }
+}
